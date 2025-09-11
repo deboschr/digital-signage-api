c0cb4ca53d6de94e09733e9a6130947b8a70f62f
deboschr <debospencher@gmail.com>
2025-09-11 07:16:43 +0700

    sa

--- internal/models/channel_model.go
+++ internal/models/channel_model.go
@@ -1,10 +1,10 @@
 package models
 
 type Channel struct {
-    ChannelID   uint   `gorm:"primaryKey;column:channel_id"`
-    Name        string `gorm:"size:100;not null;column:name"`
-    Description string `gorm:"size:255;column:description"`
-    AirportID   uint   `gorm:"not null;column:airport_id"`
-    CreatedAt   int64  `gorm:"autoCreateTime:milli;column:created_at"`
-    UpdatedAt   int64  `gorm:"autoUpdateTime:milli;column:updated_at"`
+	ChannelID   uint   `gorm:"primaryKey;column:channel_id"`
+	Name        string `gorm:"size:100;not null;column:name"`
+	Description string `gorm:"size:255;column:description"`
+	AirportID   uint   `gorm:"not null;column:airport_id"`
+	CreatedAt   int64  `gorm:"autoCreateTime:milli;column:created_at"`
+	UpdatedAt   int64  `gorm:"autoUpdateTime:milli;column:updated_at"`
 }
