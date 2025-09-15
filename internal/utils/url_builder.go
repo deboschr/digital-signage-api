8c7a1ab36afe1beb78d04e86250c1b7005330b2f
deboschr <debospencher@gmail.com>
2025-09-15 15:14:08 +0700

    test

--- internal/utils/url_builder.go
+++ internal/utils/url_builder.go
@@ -8,21 +8,29 @@ import (
 
 // BuildContentURL generates a public URL for a content file
 func BuildContentURL(title string) string {
-	baseHost := os.Getenv("APP_HOST")
-	if baseHost == "" {
-		baseHost = "localhost"
-	}
+    env := os.Getenv("APP_ENV")
+    staticPath := os.Getenv("STATIC_PATH")
+    if staticPath == "" {
+        staticPath = "/media"
+    }
 
-	basePort := os.Getenv("APP_PORT")
-	if basePort == "" {
-		basePort = "8080"
-	}
+    filename := url.PathEscape(title)
 
-	staticPath := os.Getenv("STATIC_PATH")
-	if staticPath == "" {
-		staticPath = "../media"
-	}
+    if env == "production" {
+        // production → pakai domain publik
+        return fmt.Sprintf("https://cms.pivods.com%s/%s", staticPath, filename)
+    }
 
-	filename := url.PathEscape(title)
-	return fmt.Sprintf("http://%s:%s%s/%s", baseHost, basePort, staticPath, filename)
+    // default (development / staging) → pakai host:port dari env
+    baseHost := os.Getenv("APP_HOST")
+    if baseHost == "" {
+        baseHost = "localhost"
+    }
+
+    basePort := os.Getenv("APP_PORT")
+    if basePort == "" {
+        basePort = "8080"
+    }
+
+    return fmt.Sprintf("http://%s:%s%s/%s", baseHost, basePort, staticPath, filename)
 }
