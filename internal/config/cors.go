e9eec22c10ce77bda5404acfe34980fa08ec1bed
deboschr <debospencher@gmail.com>
2025-09-20 13:34:46 +0700

    session cors 2

--- internal/config/cors.go
+++ internal/config/cors.go
@@ -1,29 +1,20 @@
 package config
 
 import (
-	"os"
-	"strings"
 	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupCORS(r *gin.Engine) {
-	// ambil origins dari environment
-	origins := os.Getenv("CORS_ORIGINS") // contoh: "https://cms.pivods.com,https://admin.pivods.com"
-	allowOrigins := []string{"http://localhost:5173"} // fallback untuk dev
-
-	if origins != "" {
-		allowOrigins = strings.Split(origins, ",")
-	}
-
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     allowOrigins,
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+   	AllowOrigins:     []string{"https://cms.pivods.com", "http://localhost:5173"},
+    	AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+    	AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+    	ExposeHeaders:    []string{"Content-Length"},
+    	AllowCredentials: true,
+    	MaxAge:           12 * time.Hour,
+    	AllowWebSockets:  true,
 	}))
+
 }
